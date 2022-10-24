2d956173a003116bd7669d42a122a0df3bb8ac69
dbrchrd <83125728+dbrchrd@users.noreply.github.com>
2022-10-24 14:59:30 +0200

    Update main.rs
    Set --help argument option

--- src/main.rs
+++ src/main.rs
@@ -13,4 +13,26 @@ fn get_program_name() -> String {
     let prog = env::args().next().unwrap();
     String::from(Path::new(&prog).file_name().unwrap().to_str().unwrap())
 }
-fn main() {}
+fn main() -> Result<()> {
+    let args: Vec<String> = env::args().collect();
+
+    let mut id_to_start: i64 = 0;
+    let mut number_to_start: i64 = 0;
+    let mut number_to_reach: i64 = 1_000_000_000;
+    let mut reset_db: bool = false;
+
+    if args.len() > 1 {
+        for el in args.clone() {
+            if el.contains("-h") || el.contains("--help") {
+                println!("Usage: {} [Options]", get_program_name());
+                println!("INTERVAL CHOICE:");
+                println!("  -f=<number> / --from=<number> : Start number");
+                println!("  -t=<number> / --to=<number>   : End number");
+                println!("DATABASE:");
+                println!("  -r / --reset-db   : Reset the database");
+            }
+        }
+    }
+
+    Ok(())
+}
