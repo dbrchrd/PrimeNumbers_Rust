2ef17c9aafef75e0df79ab878cd5079dac536591
dbrchrd <83125728+dbrchrd@users.noreply.github.com>
2022-10-24 14:56:46 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,16 @@
+use std::env;
+// use std::ffi::OsStr;
+use rusqlite::{Connection, Result, Statement};
+use std::path::Path;
 
+#[derive(Debug)]
+struct Prime {
+    id: i64,
+    value: i64,
+}
+
+fn get_program_name() -> String {
+    let prog = env::args().next().unwrap();
+    String::from(Path::new(&prog).file_name().unwrap().to_str().unwrap())
+}
+fn main() {}
